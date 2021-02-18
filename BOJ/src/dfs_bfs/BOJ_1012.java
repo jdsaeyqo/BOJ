2bd155fd2b304b5acf78419c80844d0278d19f3a
jdsaeyqo <jdsaeyqi@naver.com>
2021-02-18 16:06:23 +0900

    유기농 배추

--- BOJ/src/dfs_bfs/BOJ_1012.java
+++ BOJ/src/dfs_bfs/BOJ_1012.java
@@ -1,26 +1,6 @@
 package dfs_bfs;
 
-//2
-//10 8 17
-//0 0
-//1 0
-//1 1
-//4 2
-//4 3
-//4 5
-//2 4
-//3 4
-//7 4
-//8 4
-//9 4
-//7 5
-//8 5
-//9 5
-//7 6
-//8 6
-//9 6
-//10 10 1
-//5 5
+
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -50,28 +30,7 @@ public class BOJ_1012 {
 			return true;
 		}
 		return false;
-}
-//		
-//	static void dfs(int x, int y) {	
-//		visit[x][y] = true;
-//		
-//		
-//		for (int i = 0; i < dir.length; i++) {
-//			int new_x = x + dir[i][0];
-//			int new_y = y + dir[i][1];
-//			
-//			
-//		if(new_x >= 0 && new_x < row && new_y >= 0 && new_y < col) {
-//		
-//		if(map[new_x][new_y] == 1 && visit[new_x][new_y] == false) {
-//				dfs(new_x,new_y);
-//				
-//								
-//			}
-//		
-//		}
-//	}
-//	}	
+}	
 	
 public static void main(String[] args) throws IOException {
 	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -101,9 +60,7 @@ public static void main(String[] args) throws IOException {
 		int cnt = 0;
 		for (int k = 0; k < row; k++) {
 			for (int k2 = 0; k2 < col; k2++) {
-//			if(map[k][k2] == 1 && visit[k][k2] == false) {
-//			dfs(k,k2);
-//			cnt++;
+;
 				if(dfs(k,k2))cnt++;
 			}
 			}
