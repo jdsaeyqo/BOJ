6b74ed0be6960b68bfa206f0336004583123865a
jdsaeyqo <jdsaeyqi@naver.com>
2021-02-19 19:14:02 +0900

    토마토

--- BOJ/src/dfs_bfs/tomato.java
+++ BOJ/src/dfs_bfs/tomato.java
@@ -3,93 +3,10 @@ package dfs_bfs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
-class solution {
-	public int sol(int x, int y, int[][] map) {
-		
-		Stack<int[]> stk = new Stack<int[]>();
-		
-		int answer;
-		
-		
-		for (int i = 0; i < x; i++) {
-			for (int j = 0; j < y; j++) {
-				if(map[i][j] == 1) {
-					int a = i;
-					int b = j;
-					stk.push(new int[] {a,b});
-				}
-			}
-			
-		}
-		
-		answer = dfs(stk,map);
-		
-		for (int i = 0; i < x; i++) {
-			for (int j = 0; j < y; j++) {
-				if(map[i][j] == 0) {
-					return -1;
-					
-				}
-			}
-		}
-		
-		
-		
-		if(answer== 0) return 0;
-		
-		
-		
-		
-		
-
-		return answer-1;
-	}
-	
-	public int dfs(Stack<int[]> stk, int[][] map) {
-		
-//		boolean[][] visit = new boolean[map.length][map[0].length];
-		
-		if(stk.isEmpty()) return 0;
-		
-		Stack<int[]> newstk = new Stack<>();
-		
-		int[][] dir = new int[][] {{1,0},{-1,0},{0,1},{0,-1}};
-		
-		int cnt = 0;
-		
-		while(!stk.isEmpty()) {
-			
-			int curX = stk.peek()[0];
-			int curY = stk.peek()[1];
-			stk.pop();
-			
-			
-			for (int i = 0; i < dir.length; i++) {
-				int newX = curX + dir[i][0];
-				int newY = curY + dir[i][1];
-				
-				if(newX >=0 && newX < map.length && newY >= 0 && newY < map[0].length) {
-					if(map[newX][newY] == 0) {
-						map[newX][newY] = 1;
-
-						newstk.push(new int[] {newX,newY});
-							
-					}
-				}
-			}
-						
-		}
-		cnt++;
-		
-		cnt+=dfs(newstk,map);
-		
-
-		return cnt;
-	}
-}
 
 public class tomato {
 	
@@ -114,5 +31,84 @@ public class tomato {
 		solution s = new solution();
 		System.out.println(s.sol(row, col, map));
 		
+	}
+}
+class solution {
+	
+	public int sol(int x, int y, int[][] map) {
+		
+		Queue<int[]> q = new LinkedList<int[]>();
+		
+		int answer;
+		
+		
+		for (int i = 0; i < x; i++) {
+			for (int j = 0; j < y; j++) {
+				if(map[i][j] == 1) {
+					int a = i;
+					int b = j;
+					q.offer(new int[] {a,b});
+				}
+			}
+			
+		}
+		
+		answer = bfs(q,map);
+		
+		for (int i = 0; i < x; i++) {
+			for (int j = 0; j < y; j++) {
+				if(map[i][j] == 0) {
+					return -1;
+					
+				}
+			}
+		}		
+		
+		if(answer== 0) return 0;	
+
+		return answer-1;
+	}
+	
+	public int bfs(Queue<int[]> queue, int[][] map) {
+		
+		
+		if(queue.isEmpty()) return 0;
+		
+		Queue<int[]> q0 = new LinkedList<>();
+		
+		int[][] dir = new int[][] {{1,0},{-1,0},{0,1},{0,-1}};
+		
+		int cnt = 0;
+		
+		while(!queue.isEmpty()) {
+			
+			int[] q = queue.poll();
+			
+			int curX = q[0];
+			int curY = q[1];
+			
+			
+			
+			for (int i = 0; i < dir.length; i++) {
+				int newX = curX + dir[i][0];
+				int newY = curY + dir[i][1];
+				
+				if(newX >=0 && newX < map.length && newY >= 0 && newY < map[0].length) {
+					if(map[newX][newY] == 0) {
+						map[newX][newY] = 1;
+
+						q0.offer(new int[] {newX,newY});
+							
+					}
+				}
+			}
+						
+		}
+		cnt++;
+		
+		cnt+=bfs(q0,map);
+		
+
+		return cnt;
 	}
 }
