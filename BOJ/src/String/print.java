20151ad062377ded25bff10c1c4f6531cf112e97
jdsaeyqo <jdsaeyqi@naver.com>
2021-03-14 15:10:51 +0900

    그대로 출력하기

---
+++ BOJ/src/String/print.java
@@ -0,0 +1,25 @@
+package String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class print {
+	public static void main(String[] args) throws IOException {
+		
+		
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String str = "";
+		 
+		while ((str = br.readLine()) != null) {
+		    System.out.println(str);
+		}
+
+			
+		
+		
+	}
+}
