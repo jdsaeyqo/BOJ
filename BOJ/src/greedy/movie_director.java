613a13a9475e1638ebdf6956f4f8e0710e8c9269
jdsaeyqo <jdsaeyqi@naver.com>
2021-04-27 15:32:51 +0900

    영화감독

---
+++ BOJ/src/greedy/movie_director.java
@@ -0,0 +1,29 @@
+package greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class movie_director {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int number = 666;
+		int cnt = 1;
+		
+		while(cnt != n) {
+			number++;
+			
+			if(String.valueOf(number).contains("666")) {
+				cnt++;
+			}
+		}
+		
+	
+		System.out.println(number);
+	}
+}
